2b6e35eb84cd4a1890044b5816e07f532ac297e7
tcawill <tcawill@users.noreply.github.com>
2019-08-09 20:20:32 -0700

    Added Addtional setup for Rotation

--- RIP/Assets/Scripts/RotationTask.cs
+++ RIP/Assets/Scripts/RotationTask.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class RotationTask : MonoBehaviour
 {
+    public GameObject cubeTarget, LeftHand, RightHand;
     // Start is called before the first frame update
     void Start()
     {
-        
+        LeftHand = GameObject.FindWithTag("Left Hand");
+        RightHand = GameObject.FindWithTag("Right Hand");
+        cubeTarget = GameObject.FindWithTag("Cube");
     }
 
     // Update is called once per frame
